16b51d76eac5d54654ef0fffd9a0bdcae039e9cb
lipanre <lipanre@qq.com>
2023-03-01 22:24:14 +0800

    feat: 新增module

---
+++ java-framework/mybatis-plus/src/main/java/com/lipanre/mybatisplus/LipanreMybatisPlusApplication.java
@@ -0,0 +1,18 @@
+package com.lipanre.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * mybatisplus服务启动类
+ *
+ * @author lipanre
+ * @date 2023/3/1 22:08
+ */
+@SpringBootApplication
+public class LipanreMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LipanreMybatisPlusApplication.class, args);
+    }
+}
